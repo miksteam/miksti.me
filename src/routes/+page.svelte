facb6c945df8ba7113d181fb76ffce88facf9b7f
mikstime <35119841+mikstime@users.noreply.github.com>
2024-06-26 16:33:11 +0300

    fix(head): meta tags placed correctly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,7 @@
 	import Feed from './Feed.svelte';
 	import Header from './Header.svelte';
 	import Social from './Social.svelte';
-	import me from '$lib/images/me.jpg'
 </script>
-
-<svelte:head>
-	<title>Mikstime's Personal Page</title>
-	<meta name="description" content="Collection of articles" />
-	<meta property="og:title" content="Michael Balitsky" />
-	<meta property="og:type" content="website" />
-	<meta property="og:description" content="Frontend related person" />
-	<meta property="og:image" content={me} />
-	<meta property="og:url" content="https://miksti.me/" />
-	<meta name="twitter:card" content={me} />
-	<meta name="keywords" content="Michael Balitsky, Михаил Балицкий, mikstime">
-</svelte:head>
-
 <Header />
 <Social />
 <Feed />
