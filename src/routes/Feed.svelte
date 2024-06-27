e66f3058e53e7b9c2dc9a03ad26459388607d69f
mikstime <35119841+mikstime@users.noreply.github.com>
2024-06-27 22:14:12 +0300

    feat(demo): audio delay demo page

--- src/routes/Feed.svelte
+++ src/routes/Feed.svelte
@@ -1,11 +1,11 @@
 <section class="feed">
-	<h3 class="feed__title">MY LATEST ARTICLES</h3>
+	<h2 class="feed__title">MY LATEST ARTICLES</h2>
 	<article class="feed__post feed__post_1">
 		<a
 			class="feed__post-link"
-            target="_blank"
-            rel="noopener"
-            aria-label="How I Designed, Developed, and Deployed My Personal Website in an Evening (Mistakes Included)"
+			target="_blank"
+			rel="noopener"
+			aria-label="How I Designed, Developed, and Deployed My Personal Website in an Evening (Mistakes Included)"
 			href="https://www.linkedin.com/pulse/how-i-designed-developed-deployed-my-personal-website-balitsky-enmhe/?utm_source=share&utm_medium=guest_desktop&utm_campaign=copy"
 		>
 		</a>
@@ -13,9 +13,9 @@
 	<article class="feed__post feed__post_2">
 		<a
 			class="feed__post-link"
-            target="_blank"
-            rel="noopener"
-            aria-label="Choosing SvelteKit Over Next.js for My Next Project"
+			target="_blank"
+			rel="noopener"
+			aria-label="Choosing SvelteKit Over Next.js for My Next Project"
 			href="https://www.linkedin.com/pulse/choosing-sveltekit-over-nextjs-my-next-project-michael-balitsky-c3jte?utm_source=share&utm_medium=guest_desktop&utm_campaign=copy"
 		>
 		</a>
@@ -30,9 +30,9 @@
 		font-weight: 700;
 	}
 
-    .feed__post {
+	.feed__post {
 		margin-bottom: 32px;
-    }
+	}
 
 	.feed__post-link {
 		display: block;
@@ -50,34 +50,34 @@
 	}
 
 	@media only screen and (max-width: 800px) {
-        .feed__post {
-		    padding: 0 24px;
+		.feed__post {
+			padding: 0 24px;
 			margin-bottom: 24px;
-        }
+		}
 
 		.feed__title {
 			padding-left: 24px;
 		}
 
-        .feed__post-link {
-            border-radius: 15px;
-        }
+		.feed__post-link {
+			border-radius: 15px;
+		}
 	}
 
 	@media only screen and (max-width: 320px) {
-        .feed__post {
-		    padding: 0 16px;
+		.feed__post {
+			padding: 0 16px;
 			margin-bottom: 16px;
-        }
+		}
 	}
 
-    @media (hover: hover) {
-        .feed__post {
-            transition: transform .1s ease-in-out;
-        }
+	@media (hover: hover) {
+		.feed__post {
+			transition: transform 0.1s ease-in-out;
+		}
 
-        .feed__post:hover {
-            transform: scale(1.01);
-        }
-    }
+		.feed__post:hover {
+			transform: scale(1.01);
+		}
+	}
 </style>
--- src/routes/Social.svelte
+++ src/routes/Social.svelte
@@ -1,84 +1,85 @@
 <section class="social">
-    <h2 class="a11y-hidden">Social Media</h2>
-    <a
-        target="_blank"
-        rel="noopener"
-        aria-label="telegram profile"
-        class="social__link social__link_telegram_yes"
-        href="https://t.me/mikstime/"
-    >
-    </a>
+	<h2 class="a11y-hidden">Social Media</h2>
 	<a
-        target="_blank"
-        rel="noopener"
-        aria-label="linkedin profile"
-        class="social__link social__link_linkedin_yes"
-        href="https://www.linkedin.com/in/mikstime/"
-    >
+		target="_blank"
+		rel="noopener"
+		aria-label="telegram profile"
+		class="social__link social__link_telegram_yes"
+		href="https://t.me/mikstime/"
+	>
+	</a>
+	<a
+		target="_blank"
+		rel="noopener"
+		aria-label="linkedin profile"
+		class="social__link social__link_linkedin_yes"
+		href="https://www.linkedin.com/in/mikstime/"
+	>
 	</a>
 </section>
+
 <style>
-    .a11y-hidden {
-        font-weight: 400;
-        height: 1px;
-        left: auto;
-        overflow: hidden;
-        position: absolute;
-        top: 0;
-        width: 1px;
-        clip: rect(1px 1px 1px 1px);
-    }
+	.a11y-hidden {
+		font-weight: 400;
+		height: 1px;
+		left: auto;
+		overflow: hidden;
+		position: absolute;
+		top: 0;
+		width: 1px;
+		clip: rect(1px 1px 1px 1px);
+	}
 
 	.social {
-        display: flex;
-        margin: 0;
+		display: flex;
+		margin: 0;
 		font-size: 3.6rem;
-        padding: 0 8px 48px 32px;
+		padding: 0 8px 48px 32px;
 	}
 
 	.social__link {
-        margin-right: 24px;
-        display: block;
+		margin-right: 24px;
+		display: block;
 		width: 72px;
-        height: 72px;
-        background-size: 100%;
-        background-repeat: no-repeat;
-        background-position: center;
+		height: 72px;
+		background-size: 100%;
+		background-repeat: no-repeat;
+		background-position: center;
 	}
 
-    @media (hover: hover) {
-        .social__link {
-            transition: opacity .1s;
-        }
-        .social__link:hover {
-            opacity: .9;
-        }
-    }
-
-    .social__link_telegram_yes {
-        background-image: url($lib/images/telegram.svg);
-    }
-
-    .social__link_linkedin_yes {
-        background-image: url($lib/images/linkedin.svg);
-    }
-
-	@media only screen and (max-width: 800px) {
-        .social__link {
-            margin-right: 16px;
-	    	width: 44px;
-            height: 44px;
-	    }
-
-        .social {
-            padding-left: 24px;
-            padding-bottom: 24px;
+	@media (hover: hover) {
+		.social__link {
+			transition: opacity 0.1s;
+		}
+		.social__link:hover {
+			opacity: 0.9;
 		}
 	}
 
-    @media only screen and (max-width: 320px) {
+	.social__link_telegram_yes {
+		background-image: url($lib/images/telegram.svg);
+	}
+
+	.social__link_linkedin_yes {
+		background-image: url($lib/images/linkedin.svg);
+	}
+
+	@media only screen and (max-width: 800px) {
+		.social__link {
+			margin-right: 16px;
+			width: 44px;
+			height: 44px;
+		}
+
 		.social {
-            padding-left: 16px;
+			padding-left: 24px;
+			padding-bottom: 24px;
+		}
+	}
+
+	@media only screen and (max-width: 320px) {
+		.social {
+			padding-left: 16px;
 		}
 	}
 </style>
